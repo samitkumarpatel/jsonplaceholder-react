21b0c8d0fdf1b3f1d0d286dab6e6877fcbf42b0e
samitkumarpatel <samitkumarpatel@gmail.com>
2024-07-19 07:57:09 +0000

    fix

--- src/views/Users.js
+++ src/views/Users.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { redirect, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Users = () => {
     const [users, setUsers] = useState([])
